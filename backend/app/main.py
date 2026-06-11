9b79a2422f568e1fc5faa59ec74e2b300421de80
Arpita Garg <arpitagarg1812@gmail.com>
2026-06-11 13:18:56 +0530

    adding CORS to fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.api.routes import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
-print("USING FULL API VERSION")
 app = FastAPI(title="Domain Knowledge Copilot API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://corpusiq.onrender.com",
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
